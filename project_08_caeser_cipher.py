0ee62ce959a11e7ced85d98c94a6e736bd6c24ab
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-01 12:27:10 -0300

    update

--- project_08_caeser_cipher.py
+++ project_08_caeser_cipher.py
@@ -1,22 +1,26 @@
 #caeser cipher
-
+from caeser_cipher_art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caeser(start_text, shift_amount, cypher_direction):
     end_text = ""
-
     if cypher_direction == "decode":
         shift_amount *= -1
 
     for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        end_text += new_letter
-    print(f"The {which_direction} text is {end_text}.")
+        if letter not in alphabet:
+            new_letter = letter
+            end_text += new_letter
+        else:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            end_text += new_letter
+    print(f"The {cypher_direction} text is {end_text}.")
 
 caeser(start_text=text, shift_amount=shift, cypher_direction=direction)
