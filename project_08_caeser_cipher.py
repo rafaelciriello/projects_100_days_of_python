43b3074bac11578b24574db37ee2a942e3283ab3
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-01 11:32:11 -0300

    update

--- project_08_caeser_cipher.py
+++ project_08_caeser_cipher.py
@@ -6,25 +6,18 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
+def caeser(plain_text, shift_amount, which_direction):
     cipher_text = ""
+
+    if which_direction == "decode":
+        shift_amount = shift_amount * -1
+
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
         new_letter = alphabet[new_position]
         cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+    print(f"The {which_direction} text is {cipher_text}")
 
-def decrypt(cypher_text, shift_amount):
-    plain_text = ""
-    for letter in cypher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
+caeser(plain_text=text, shift_amount=shift, which_direction=direction)
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-else:
-    decrypt(cypher_text=text, shift_amount=shift)
