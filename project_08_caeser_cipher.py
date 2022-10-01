1d7794d4bdb465a30ec73296af3bb6a7587dd48c
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-01 09:37:57 -0300

    update

--- project_08_caeser_cipher.py
+++ project_08_caeser_cipher.py
@@ -8,12 +8,12 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
+#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+#e.g.
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+#print output: "The encoded text is mjqqt"
 
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
