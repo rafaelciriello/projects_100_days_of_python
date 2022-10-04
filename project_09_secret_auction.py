522e301e959836da583f4d9e6f82c6d7ca02ea1e
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-04 16:21:06 -0300

    update

--- project_09_secret_auction.py
+++ project_09_secret_auction.py
@@ -1,14 +1,24 @@
-#dictionary
+#secret_auction
 
-travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visited": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visited": 6,
-    },
-]
+from secret_auction_art import logo
+bids = {}
+
+def added_bid(user_name, user_bid):
+    bids[user_name] = user_bid
+
+print(logo)
+print("Welcome to the secret auction program.")
+
+end_the_action = False
+
+while not end_the_action:
+    user = input("What is your name? ")
+    bid = input("What's your bide? $ ")
+    new_user = input("New user? 'Y' or 'N': ").lower()
+
+    if new_user == "n":
+        end_the_action = True
+
+    added_bid(user_name=user, user_bid=bid)
+
+print(bids)
