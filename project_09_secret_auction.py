589cfe5b0b01451e75e115edb740d2ca6bd5c909
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-04 10:02:37 -0300

    update

---
+++ project_09_secret_auction.py
@@ -0,0 +1,14 @@
+#dictionary
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visited": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visited": 6,
+    },
+]
