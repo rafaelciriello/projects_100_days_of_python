a50eee65cb8acab49e09c454c05be5b389ceeaa1
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-05 09:57:22 -0300

    update

--- project_09_secret_auction.py
+++ project_09_secret_auction.py
@@ -5,9 +5,14 @@ print(logo)
 print("Welcome to the secret auction program.")
 
 bids = {}
+bidding_finished = False
 
-name = input("What is your name? ")
-price = input("What's your bide? $")
-bids[name] = price
+while not bidding_finished:
+    name = input("What is your name? ")
+    price = input("What's your bide? $")
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    if should_continue == "no":
+        bidding_finished = True
 
 print(bids)
