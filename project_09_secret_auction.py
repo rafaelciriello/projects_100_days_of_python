d2d824cf99d461b6b20084233c7e060b9234cc8b
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-05 10:10:17 -0300

    def find_highest_bidder():

--- project_09_secret_auction.py
+++ project_09_secret_auction.py
@@ -7,6 +7,16 @@ print("Welcome to the secret auction program.")
 bids = {}
 bidding_finished = False
 
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
 while not bidding_finished:
     name = input("What is your name? ")
     price = input("What's your bide? $")
