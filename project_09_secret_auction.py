d13d720d85e8c219365d70d3a26b0768f1ef934d
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-05 09:51:16 -0300

    update

--- project_09_secret_auction.py
+++ project_09_secret_auction.py
@@ -1,24 +1,13 @@
 #secret_auction
 
 from secret_auction_art import logo
-bids = {}
-
-def added_bid(user_name, user_bid):
-    bids[user_name] = user_bid
-
 print(logo)
 print("Welcome to the secret auction program.")
 
-end_the_action = False
+bids = {}
 
-while not end_the_action:
-    user = input("What is your name? ")
-    bid = input("What's your bide? $ ")
-    new_user = input("New user? 'Y' or 'N': ").lower()
-
-    if new_user == "n":
-        end_the_action = True
-
-    added_bid(user_name=user, user_bid=bid)
+name = input("What is your name? ")
+price = input("What's your bide? $")
+bids[name] = price
 
 print(bids)
