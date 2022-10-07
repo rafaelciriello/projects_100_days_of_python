36db9264ae6f8bda01b25762a842aca669e1efe1
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-07 15:16:36 -0300

    update

--- project_10_calculator.py
+++ project_10_calculator.py
@@ -22,12 +22,21 @@ operations = {
     "/": divide,
     }
 
-num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+while True:
+    num1 = int(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} '=' {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ").lower() == "y":
+            num1 = answer
+        else:
+            should_continue = False
