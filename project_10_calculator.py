7ecf621f09cb219a9908eee3fb7b633d1ef0eaed
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-07 14:50:11 -0300

    update

--- project_10_calculator.py
+++ project_10_calculator.py
@@ -4,14 +4,30 @@ def add(n1, n2):
     return n1 + n2
 
 #subtract
-def sub(n1, n2):
+def subtract(n1, n2):
     return n1 - n2
 
 #multiply
-def mul(n1, n2):
+def multiply(n1, n2):
     return n1 * n2
 
 #divide
-def div(n1, n2):
+def divide(n1, n2):
     return n1 / n2
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    }
+
+num1 = int(input("What's the first number? "))
+num2 = int(input("What's the second number? "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+
+
+
+print(f"{num1} {operation_symbol} {num2} '=' {answer}")
