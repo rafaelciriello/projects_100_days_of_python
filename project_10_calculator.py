3b288e2d3fdcf51bf1a058004f44d66db09de21c
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-07 14:15:32 -0300

    update

---
+++ project_10_calculator.py
@@ -0,0 +1,17 @@
+#calculator
+#add
+def add(n1, n2):
+    return n1 + n2
+
+#subtract
+def sub(n1, n2):
+    return n1 - n2
+
+#multiply
+def mul(n1, n2):
+    return n1 * n2
+
+#divide
+def div(n1, n2):
+    return n1 / n2
+
