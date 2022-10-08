98e930002357cebfbd8df42691a6d566e3a1a7ab
Rafael Ciriello <ciriello.dev@gmail.com>
2022-10-07 21:46:39 -0300

    update

--- 011_project_blackjack.py
+++ 011_project_blackjack.py
@@ -2,26 +2,20 @@
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-computer_cards = []
 user_cards = []
+computer_cards = []
 
-#selecting two random initial cards
+#Create a deal_card() function that uses the List below to *return* a random card.
+def deal_card():
+    '''uses the list to return a random card'''
+    card = random.choice(cards)
+    return card
+
+#Deal the user and computer 2 cards each using deal_card() and append().
 for number in range(0, 2):
-    computer_cards.append(random.choice(cards))
-    user_cards.append(random.choice(cards))
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
-print(f"The first card in the computer is: {computer_cards[0]}")
-print(f"The first card in the user is: {user_cards[0]}")
-
-#adding up the value of the cards
-total_computer = sum(computer_cards)
-total_user = sum(user_cards)
-
-print(total_computer, total_user)
-
-#check that the result is equal to 21
-if total_computer == 21:
-    print("You lose")
-elif total_user == 21:
-    print("you win")
+#Create a function called calculate_score() that takes a List of cards as input and return the score.
+def calculate_score():
+    return sum()
